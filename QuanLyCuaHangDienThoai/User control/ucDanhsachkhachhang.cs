156d9f30a34faa7f88da6729f90e69a16cc45768
HuynhNguyenChauDuy <phuc210495@gmail.com>
2015-12-09 17:06:59 +0700

    Insert ucDanhsachkhachhang

---
+++ QuanLyCuaHangDienThoai/User control/ucDanhsachkhachhang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangDienThoai.User_control
+{
+    public partial class ucDanhsachkhachhang : UserControl
+    {
+        public ucDanhsachkhachhang()
+        {
+            InitializeComponent();
+        }
+    }
+}
